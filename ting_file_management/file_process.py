e48765d1e421f573fa207b11f67ad50644ab5558
karl-no <karl.publicidade@gmail.com>
2023-05-08 14:06:28 -0300

    feat: process function, in file_process file, capable of transforming the content from a list into a dictionary that is stored in the queue

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    processed_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+
+    for data in instance._data:
+        if data["nome_do_arquivo"] == path_file:
+            return False
+
+    instance.enqueue(processed_data)
+
+    return sys.stdout.write(str(processed_data))
 
 
 def remove(instance):
