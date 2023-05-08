e96a49243b09b9c34e3a8711c9f2fbfecb7a81a9
karl-no <karl.publicidade@gmail.com>
2023-05-08 13:29:51 -0300

    feat: text_importer function is now able to import a .txt file

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith('txt'):
+        try:
+            with open(path_file) as file:
+                return file.read().split('\n')
+        except FileNotFoundError:
+            return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    else:
+        sys.stderr.write("Formato inválido")
