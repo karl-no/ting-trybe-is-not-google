4e93dc8640fe3269c800bdb3334f5b363714a9e0
karl-no <karl.publicidade@gmail.com>
2023-05-08 18:45:56 -0300

    feat: exists_word function in word_search.py implemented

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    list_of_words = []
+
+    for data in instance._data:
+        for line in range(len(data["linhas_do_arquivo"])):
+            if word.lower() in data["linhas_do_arquivo"][line].lower():
+                list_of_words.append({"linha": line + 1})
+
+        if len(list_of_words):
+            return [{
+                "palavra": word,
+                "arquivo": data["nome_do_arquivo"],
+                "ocorrencias": list_of_words
+                }]
+
+        return []
 
 
 def search_by_word(word, instance):
