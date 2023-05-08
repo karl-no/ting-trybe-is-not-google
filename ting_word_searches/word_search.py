f908c4b6fbd0866fd9080bb96f796170fa9fe49f
karl-no <karl.publicidade@gmail.com>
2023-05-08 19:14:09 -0300

    feat: search_by_word function, in word_search.py file, implemented

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -17,4 +17,28 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    list_of_words = []
+
+    for data in instance._data:
+        dictionary = {
+                "palavra": word,
+                "arquivo": data["nome_do_arquivo"],
+                "ocorrencias": [],
+                }
+
+        count = 1
+
+        for line in data["linhas_do_arquivo"]:
+            if word.lower() in line.lower():
+                dictionary["ocorrencias"].append({
+                    "linha": count,
+                    "conteudo": line,
+                })
+            count += 1
+
+        if len(dictionary["ocorrencias"]):
+            list_of_words.append(dictionary)
+
+        dictionary["ocorrencias"] == []
+
+        return list_of_words
