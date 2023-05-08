4336afd5141977f9a6e07fce736e7a64edd0c5b0
karl-no <karl.publicidade@gmail.com>
2023-05-08 17:52:26 -0300

    test: tests for PriorityQueue class done!

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,19 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
+
+priority_file = {"qtd_linhas": 3}
+non_priority_file = {"qtd_linhas": 6}
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority = PriorityQueue()
+
+    priority.enqueue(non_priority_file)
+    assert priority.dequeue() == non_priority_file
+
+    priority.enqueue(non_priority_file)
+    priority.enqueue(priority_file)
+    assert priority.search(0) == priority_file
+
+    with pytest.raises(IndexError):
+        priority.search(7)
